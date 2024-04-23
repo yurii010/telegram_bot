86acc2baeccaa401f1a8919add6351e23a705a59
Yurii <125599335+yurii010@users.noreply.github.com>
2024-04-23 21:44:14 +0300

    add bot

---
+++ index.js
@@ -0,0 +1,70 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const token = '7187652540:AAEZ4YmQcESjSCttTnRmTWfwTKnfBXGupqw';
+const webAppUrl = "https://main--dashing-buttercream-8dc15b.netlify.app/";
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+        await bot.sendMessage(chatId, "Заповніть форму нижче", {
+            reply_markup: {
+                keyboard: [
+                    [{ text: 'Відкрити форму', web_app: { url: webAppUrl + 'form' } }]
+                ]
+            }
+        })
+        await bot.sendMessage(chatId, "Інтернет магазин", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'Зробити замовлення', web_app: { url: webAppUrl } }]
+                ]
+            }
+        })
+    }
+
+    if (msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data);
+            await bot.sendMessage(chatId, 'Your country: ' + data?.country);
+            await bot.sendMessage(chatId, 'Your country: ' + data?.city);
+            await bot.sendMessage(chatId, 'Your country: ' + data?.subject);
+            await bot.sendMessage(chatId, 'Thank you!');
+        } catch (e) {
+            console.log(e);
+        }
+    }
+});
+
+app.post('/web-data', async (req, res) => {
+    const { queryId, products, totalPrice } = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Successful',
+            input_message_content: { message_text: `Congratulation ${totalPrice}` },
+        });
+        return res.status(200).json({})
+    } catch (e) {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Unsuccessful(',
+            input_message_content: { message_text: `Unsuccessful(` },
+        });
+    }
+    return res.status(500).json({})
+})
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`server on ${PORT}`);
+});
