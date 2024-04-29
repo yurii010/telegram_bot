17d7715322380f09ea65bd56a269f24c139a3cd7
Yurii <125599335+yurii010@users.noreply.github.com>
2024-04-29 15:54:13 +0300

    clear

--- sequelize/info.js
+++ sequelize/info.js
@@ -1,14 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('bot', 'root', '',
-    {
-        host: '127.0.0.1',
-        dialect: 'mysql'
-    }
-);
-
-sequelize.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
-})
+const sequelize = new Sequelize('bot', 'root', '', { host: '127.0.0.1', dialect: 'mysql' });
 
 const usersInfo = sequelize.define("usersInfo", {
     userId: {
@@ -28,11 +19,11 @@ const usersInfo = sequelize.define("usersInfo", {
 
 sequelize.sync().then(() => {
     console.log('Book table created successfully!');
- 
+
     usersInfo.create({
         userId: 1,
         username: "test",
         first_name: "test",
         language_code: "test"
     })
- })
+})
